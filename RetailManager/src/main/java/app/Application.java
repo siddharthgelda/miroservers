1db3d29d245191ce39e5362dd0797adfd2f4fe78
siddharthgelda <geldasiddharth@gmail.com>
2016-05-17 14:34:50 +0530

    Update Application.java

--- RetailManager/src/main/java/app/Application.java
+++ RetailManager/src/main/java/app/Application.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @ComponentScan("app")
 @SpringBootApplication 
-//@EnableEurekaClient
+@EnableEurekaClient
 public class Application {
 
   public static void main(String[] args) throws Exception {
